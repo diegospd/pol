6850b7bbfb3fe5410fc39cacc0de885a1f0ef195
dieg <diego.spd@gmail.com>
2017-01-15 14:26:41 -0600

    better ui

--- src/AppDraw.hs
+++ src/AppDraw.hs
@@ -44,7 +44,7 @@ helpUI = [ ( "Esc"            , "quit no save"  )
 
 
 displayHelp :: Bool -> Widget n
-displayHelp True = hCenter $ twoColumns helpUI
+displayHelp True  = hCenter $ twoColumns helpUI
 displayHelp False = padLeft (Pad 3) $ str offerHelp
     where offerHelp = "Press h to show help"
 
@@ -75,15 +75,44 @@ lastSave st
 
 myDraw :: PState -> [Widget N]
 myDraw st = do
-    let list  = renderList' st 
-    let debug = info st
+    let list  = drawList st 
+    let debug = drawInfo st
     let saved = padLeft (Pad 3) . padTopBottom 1 $ lastSave st
     let help  = displayHelp (st^.showingHelp)
     return $ vBox [list, help, saved, debug]
 
 
-renderList' :: PState -> Widget N
-renderList' st 
+
+
+
+----------------------------------------------------------
+
+
+drawDepth :: (Entry, Zipper) -> Widget n
+drawDepth (e, z)
+    | isLast z && (e^.itsDepth) > 1 = hBox $ (replicate (e^.itsDepth - 2) step) ++ [last]
+    | otherwise = hBox $  replicate (e^.itsDepth - 1) step 
+       where step  = str "  │  "
+             last  = str "  └──"
+
+
+drawText :: Entry -> Widget n
+drawText e = case e^.itsDepth of
+    1 -> txt $ "[" <> e^.itsText <> "]"
+    _ -> txt (e^.itsText)
+
+
+
+drawBullet :: (Entry, Zipper) -> Widget n
+drawBullet (e, z) 
+    | e^.isCollapsed && hasChildren z = str "  + "
+    | hasChildren z                   = str "  ♦ "
+    | otherwise                       = str "  · "
+
+
+
+drawList :: PState -> Widget N
+drawList st 
     | isEmpty (st^.theTree) = center $ str "  Such an empty tree..." <=> str "Press 'a' to add and entry"
     | otherwise = padAll 2  ls
     where ls = myBorder $ renderList (renderEntry st) True (st^.theList) 
@@ -91,35 +120,35 @@ renderList' st
                      then withBorderStyle unicodeBold . border . border
                      else withBorderStyle unicode . border . withBorderStyle (borderStyleFromChar ' ') . border
 
+
+
+drawDecoration :: (Entry, Zipper) -> Widget n 
+drawDecoration (e,z) =  drawDepth (e,z) <+>  drawBullet (e,z)
+
 renderEntry :: PState -> Bool -> (Entry, Zipper) -> Widget N
-renderEntry _ False (e,z) = padDepth e  (renderEntry' (e,z))
+renderEntry _ False (e,z) = drawDecoration (e,z)  <+> drawText e
 renderEntry st True (e,z) 
-    | st^.inEditMode = padDepth e $ renderEditor True (st ^. theEditor)
-    | otherwise = padDepth e (renderEntry' (e,z))
+    | st^.inEditMode = drawDecoration (e,z) <+> renderEditor True (st ^. theEditor)
+    | otherwise = drawDecoration (e,z) <+> drawText e
 
-padDepth :: Entry -> Widget n -> Widget n
-padDepth e = padLeft (Pad $ 4 * (e^.itsDepth))
 
+
+
+
+-- | Looks ugly; won't use for now.
 tagWithNumOfSons :: Zipper -> Widget n
 tagWithNumOfSons z = str $ "  [" <> fstLvl <> "|" <> total <> "]" 
    where fstLvl = maybe "0" (show . length)  ((!!! 1) . levels . tree $ z) 
          total = show . subtract 1 . length . flatten . tree $ z
 
-renderEntry' :: (Entry, Zipper) -> Widget n
-renderEntry' (e,z) = text <+> tagWithNumOfSons z
-    where text = case e^.itsDepth of
-            1 -> txt $ "[" <> e^.itsText <> "]"
-            2 -> txt $ "- " <> e^.itsText 
-            3 -> txt $ "· " <> e^.itsText 
-            _ -> txt (e^.itsText)
 
 
 
 ------------------------debug--------------------------------------
 
 
-info :: PState -> Widget N
-info st = if debug_flag then info' st else emptyWidget
+drawInfo :: PState -> Widget N
+drawInfo st = if debug_flag then info' st else emptyWidget
 
 info' :: PState -> Widget N
 info' st = case listSelectedElement (st^.theList) of
