9629f7d18a9c3d89506c4e080bf43edc08de7595
diego murillo <diego.albarran@nubank.com.br>
2020-06-26 12:14:35 -0500

    builds

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,14 @@ module Main where
 
 import Options.Applicative as OA
 import Options.Applicative ((<**>))
-import Data.Monoid ((<>))
+import Data.Monoid         ((<>))
 
 import Types.Base
 import Prelude hiding(FilePath)
-import Types.CliArguments (CliArgs(..))
+import Types.CliArguments  (CliArgs(..))
 import Types.EertArguments (AppArgs(..))
 
-import qualified Adapter.Arguments as Arguments
+import Adapter.Arguments as Arguments
 import StartApp(runTheApp)
 
 
@@ -18,19 +18,19 @@ main :: IO ()
 main = operationMode =<< Arguments.build <$> OA.execParser opts
   where
     opts = OA.info (sample <**> OA.helper)
-      ( OA.fullDesc
-     <> OA.progDesc "Console GUI tree based note taker"
-     <> OA.header   "you can choose which eert file to load" )
+         ( OA.fullDesc
+        <> OA.progDesc "Console GUI tree based note taker"
+        <> OA.header   "you can choose which eert file to load" )
 
 sample :: OA.Parser CliArgs
 sample = CliArgs
       <$> OA.many (OA.argument OA.str (OA.metavar "FILE"))
       <*> OA.strOption
-             ( OA.long "filename"
-            <> OA.short 'f'
+             ( OA.long    "filename"
+            <> OA.short   'f'
             <> OA.metavar "FILE"
-            <> OA.value "~/.eert.main.json"
-            <> OA.help "Loads tree in FILE" )
+            <> OA.value   "~/.eert.main.json"
+            <> OA.help    "Loads tree in FILE" )
 
 operationMode (AppArgs filename) = runTheApp filename
 
--- app/Types/EertArguments.hs
+++ app/Types/EertArguments.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Types.EertArguments where
 
-import Types.Base (FilePath)
+import Types.Base    (FilePath)
 import Prelude hiding(FilePath)
 
 newtype AppArgs = AppArgs {filename :: FilePath}
