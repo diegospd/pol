5ec489e680f538f1f49cc3e39f0670fd0b012d83
diego murillo <diego.albarran@nubank.com.br>
2020-06-26 12:47:58 -0500

    pretty

--- src/Adapter/Tree.hs
+++ src/Adapter/Tree.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Adapter.Tree where
 
-import Types.ETree
-import Types.Base
-import Prelude hiding (FilePath)
-import Types.EState
-import Adapter.Entry as Entry
-import Logic.ETree as ETree
+import           Prelude       hiding (FilePath)
+import           Types.Base
+import           Types.ETree
+import           Types.EState
+import qualified Adapter.Entry as Entry
+import qualified Logic.ETree   as ETree
+
 
 textTreeToETree :: Tree Text -> ETree
 textTreeToETree = ETree.fixTree . fmap Entry.fromText
