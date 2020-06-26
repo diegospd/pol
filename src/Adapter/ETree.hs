6afae540c5f06468cdf0de431450565c80d49173
diego murillo <diego.albarran@nubank.com.br>
2020-06-26 12:42:46 -0500

    builds

--- src/Adapter/ETree.hs
+++ src/Adapter/ETree.hs
@@ -8,21 +8,15 @@ import Types.ETree
 import Types.EState
 import Types.Brick as Brick
 
-import qualified Data.Vector as V
-import qualified Logic.Tree as T
+import qualified Data.Vector  as V
+import qualified Logic.Tree   as T
 import qualified Logic.Zipper as Z
 
 
 toTree :: ETree -> Tree (Entry, Zipper)
-toTree t = seZippers (Z.fromTree t) t
+toTree t = Z.zipETree (Z.fromTree t) t
 
 toList :: ETree -> Brick.List Brick.N (Entry, Zipper)
 toList t = list "theList" es 1
     where es = V.fromList $ filter ((^.isVisible) . fst) $ T.flatten $ toTree t
     -- where es = V.fromList $ {-filter (^.isVisible) $-} flatten $ toTree t
-
-seZippers :: Zipper -> ETree -> Tree (Entry, Zipper)
-seZippers z (Node e ts) = Node (e, z) (zipWith seZippers zs ts)
-    where zs = [fromJust $ Z.childAt (n-1) z | n <- [1 .. length ts] ]
-
-
--- src/Logic/Zipper.hs
+++ src/Logic/Zipper.hs
@@ -1,12 +1,18 @@
 module Logic.Zipper
-  ( isFirstLevelOrRoot,
-    module X
+  ( isFirstLevelOrRoot
+  , zipETree
+  , module X
   )
 where
 
+import Data.Maybe
 import Data.Tree.Zipper as X
+import Types.ETree as ETree
 
-import Types.ETree(Zipper(..))
 
-isFirstLevelOrRoot :: Zipper -> Bool
+isFirstLevelOrRoot :: ETree.Zipper -> Bool
 isFirstLevelOrRoot z = maybe True X.isRoot (X.parent z)
+
+zipETree :: ETree.Zipper -> ETree -> Tree (Entry, Zipper)
+zipETree z (Node e ts) = Node (e, z) (zipWith zipETree zs ts)
+    where zs = [fromJust $ X.childAt (n-1) z | n <- [1 .. length ts] ]
