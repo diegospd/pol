8e1fef113f973c0f5b698af0f0d0fc6d282f10f7
diego murillo <diego.albarran@nubank.com.br>
2020-06-26 12:51:50 -0500

    pretty

--- src/Adapter/ETree.hs
+++ src/Adapter/ETree.hs
@@ -4,10 +4,10 @@ module Adapter.ETree where
 
 import Prelude hiding (FilePath)
 import Types.Base
-import Types.ETree
+import Types.ETree as ETree
 import Types.EState
-import Types.Brick as Brick
 
+import qualified Types.Brick  as B
 import qualified Data.Vector  as V
 import qualified Logic.Tree   as T
 import qualified Logic.Zipper as Z
@@ -16,7 +16,6 @@ import qualified Logic.Zipper as Z
 toTree :: ETree -> Tree (Entry, Zipper)
 toTree t = Z.zipETree (Z.fromTree t) t
 
-toList :: ETree -> Brick.List Brick.N (Entry, Zipper)
-toList t = list "theList" es 1
-    where es = V.fromList $ filter ((^.isVisible) . fst) $ T.flatten $ toTree t
-    -- where es = V.fromList $ {-filter (^.isVisible) $-} flatten $ toTree t
+toList :: ETree -> B.List B.N (Entry, Zipper)
+toList t = B.list "theList" es 1
+    where es = V.fromList $ filter ((^. ETree.isVisible) . fst) $ T.flatten $ toTree t
