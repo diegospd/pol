58ee82fbd4cb1eb2900fcaa7bef3a4015a03e328
diego murillo <diego.albarran@nubank.com.br>
2020-06-25 21:55:17 -0500

    wip

--- src/Start.hs
+++ src/Start.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Start
     ( someFunc
     ) where
 
+import Types.Base
+import Types.EState
+import Types.ETree
+import Adapter.ETree as ETree
+import App
+import qualified App.IO
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = runTheApp
+
+emptyTree :: ETree
+emptyTree = ETree.fixTree $ Node (textToEntry "root") []
+
+runTheApp :: IO ()
+runTheApp = do
+    fromDisk <- App.IO.readTree
+    let tree = fromMaybe emptyTree fromDisk
+    let st   = ETree.toState tree & lastSavedTree ?~ tree
+    void $ defaultMain theApp st
+
+
+mkState :: Tree Text -> EState
+mkState = toState .  textTreeToETree
