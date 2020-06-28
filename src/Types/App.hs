7d3c2ba631c4af360bd99ef3440d5a1faf0c2084
diego murillo <diego.albarran@nubank.com.br>
2020-06-28 14:35:55 -0500

    builds. simple reader

--- src/Types/App.hs
+++ src/Types/App.hs
@@ -1,17 +1,18 @@
 module Types.App where
 
 import qualified Types.Brick as B
+import Types.Base
 import Types.AppConfig(Config(..))
-
-import Control.Monad.Catch (MonadThrow, MonadCatch, MonadMask)
-import Control.Monad.Trans.State.Lazy
-import Control.Monad.Fail (MonadFail)
+--
+--import Control.Monad.Catch (MonadThrow, MonadCatch, MonadMask)
+--import Control.Monad.Trans.State.Lazy
+--import Control.Monad.Fail (MonadFail)
 import Control.Monad.Trans.Reader
-import Control.Monad.IO.Class
-
-
+--import Control.Monad.IO.Class
+--
+--
 --newtype GuiM a =
---    GuiM { runGui :: ReaderT Config (runEventM $ B.EventM B.N a)
+--    GuiM { runGui :: ReaderT Config (B.EventM Text a)
 --           }
 --           deriving ( Functor
 --                    , Applicative
@@ -22,3 +23,27 @@ import Control.Monad.IO.Class
 --                    , MonadMask
 --                    , MonadFail
 --                    )
+
+
+{-# LANGUAGE DeriveFunctor #-}
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+import Data.Functor.Identity
+
+newtype GuiM a = GuiM {runGuiM :: Config -> ReaderT Config Identity a}
+
+--runReader :: GuiM a -> Config -> a
+--runReader guiM config = (runIdentity . runReaderT guiM) config
+
+askConfig :: Monad m => ReaderT r m r
+askConfig = undefined
+
+main :: IO ()
+main = runReaderT main' "Hello World"
+
+main' :: ReaderT String IO ()
+main' = do
+  lift $ putStrLn "I'm going to tell you a message"
+  liftIO $ putStrLn "The message is:"
+  message <- ask
+  lift $ putStrLn message
