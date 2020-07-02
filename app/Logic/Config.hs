a1f432732a0aea75ad1f1f55b19106dec68d667a
diego murillo <diego.albarran@nubank.com.br>
2020-07-02 17:15:30 -0500

    builds

--- app/Logic/Config.hs
+++ app/Logic/Config.hs
@@ -8,5 +8,7 @@ import Types.AppConfig
 import Adapter.Config as Config
 
 build :: FilePath -> Maybe LocalConfig -> CliArgs -> Config
-build defaultSaveFile Nothing (CliArgs [] verbose) = Config defaultSaveFile verbose
---build defaultSaveFile _ (CliArgs [] verbose) = Config defaultSaveFile verbose
+build defaultSaveFile Nothing                                (CliArgs [] verbose)         = Config defaultSaveFile verbose
+build defaultSaveFile (Just (LocalConfig (Just saveFile) _)) (CliArgs [] verbose)         = Config saveFile verbose
+build defaultSaveFile _localConfig                           (CliArgs [saveFile] verbose) = Config saveFile verbose
+build defaultSaveFile _localConfig                           _cliArgs                     = Config defaultSaveFile False
