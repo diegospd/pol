a648fe656f9bff9ad525f782065f5ee0608c09d4
diego murillo <diego.albarran@nubank.com.br>
2020-07-02 23:30:16 -0500

    pedantic builds

--- app/Main.hs
+++ app/Main.hs
@@ -2,60 +2,67 @@
 
 module Main where
 
-import Data.Monoid ((<>))
 import IO.LocalConfig (loadLocalConfig)
 import qualified Options.Applicative as OA
 import StartApp (runTheApp)
 import qualified Turtle as Sh
 import Types.AppConfig
-import qualified Types.AppConfig as Config
 import qualified Logic.Config as Config
 import Types.Base
 import Types.CliArguments (CliArgs (..))
 import Prelude hiding (FilePath)
 
+filenameDefaultSaveFile :: FilePath
+filenameDefaultSaveFile = ".pol.tree.json"
+
+filenameLocalConfig :: FilePath
+filenameLocalConfig = ".pol.conf"
+
+
 -- | (LocalConfig, default saveFile)
-defaultFileLocations :: IO (FilePath, FilePath)
-defaultFileLocations = do
+defaultFullFilePaths :: IO (FilePath, FilePath)
+defaultFullFilePaths = do
   homeDirectory <- Sh.home
-  return ( homeDirectory </> ".pol.conf"
-         , homeDirectory </> ".pol.tree.json")
+  return ( homeDirectory </> filenameLocalConfig
+         , homeDirectory </> filenameDefaultSaveFile
+         )
 
 main :: IO ()
 main = do
   (pathLocalConfig
-   ,pathDefaultSaveFile) <- defaultFileLocations
+   ,pathDefaultSaveFile) <- defaultFullFilePaths
   maybeLocalConfig       <- loadLocalConfig pathLocalConfig
-  cliArgs                <- OA.execParser (cliParser undefined)
+  cliArgs                <- OA.execParser cliParser
   let config             =  Config.build pathDefaultSaveFile maybeLocalConfig cliArgs
   operationMode config
 
-cliParser :: FilePath -> OA.ParserInfo CliArgs
-cliParser homeDir =
+cliParser :: OA.ParserInfo CliArgs
+cliParser =
   OA.info
-    (cliInstructions homeDir OA.<**> OA.helper)
+    (cliInstructions OA.<**> OA.helper)
     (  OA.fullDesc
-    <> OA.header   "POL - Console GUI tree-based note taker"
-    <> OA.progDesc "Launches the Pol GUI loading FILE or default "
+    <> OA.header    "POL - Console GUI tree-based note taker"
+    <> OA.progDesc ("Launches the Pol GUI loading FILE or defaults to '~/"
+                   <> Sh.encodeString filenameDefaultSaveFile
+                   <> "' when no FILE argument is provided.")
     )
 
-cliInstructions :: FilePath -> OA.Parser CliArgs
-cliInstructions homeDir =
+cliInstructions :: OA.Parser CliArgs
+cliInstructions =
   CliArgs
-    <$> OA.many (OA.argument OA.str
-                             (OA.metavar "FILE"))
+    <$> OA.many (OA.argument
+                    OA.str
+                    (OA.metavar "FILE"))
     <*> OA.switch
-      ( OA.long "verbose"
+      (   OA.long  "verbose"
        <> OA.short 'v'
-       <> OA.help "Enable verbose mode" )
-
--- | Show options, no gui
-greet :: Config -> IO ()
-greet conf = do
-   print conf
-
+       <> OA.help  "Enable verbose mode" )
 
 operationMode :: Config -> IO ()
 operationMode conf = do
   greet conf
   runTheApp conf
+
+greet :: Config -> IO ()
+greet conf = do
+   print conf
